eb4b788f75a8e63017e569ee0a15869e45b9eacf
Cherry-pellets <50487156+Cherry-pellets@users.noreply.github.com>
2020-02-07 14:10:41 +0800

    商品页头部的实现

--- sell/src/App.vue
+++ sell/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-header></v-header>
+    <v-header :seller="seller"></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
         <router-link to="/goods">商品</router-link>
@@ -16,8 +16,10 @@
   </div>
 </template>
 
-<script>
+<script type="text/ecmascript-6">
     import header from './components/header/header.vue';
+
+    const ERR_OK = 0;
 
     export default {
         /* name: 'App', */
@@ -25,6 +27,17 @@
             return {
                 seller: {}
             };
+        },
+        created() {
+            this.$http.get('/api/seller').then((response) => {
+                response = response.body;
+                // console.log(response);
+                if (response.errno === ERR_OK) {
+                    this.seller = response.data;
+                    // console.log(response.seller);
+                    // console.log(this.seller);
+                }
+            });
         },
         components: {
             'v-header': header
